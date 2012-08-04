f6da5c8b5557ddefd27a06c39ae349428cff9b54
Siddhartha Dugar <dugar.siddhartha@gmail.com>
2012-08-04 07:46:43 +0530

    Use increased font and icon size on large displays

--- bootstrap.js
+++ bootstrap.js
@@ -1,28 +1,33 @@
-const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
-let sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-let ios = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
+var Cc = Components.classes, Ci = Components.interfaces;
+var sss = Cc['@mozilla.org/content/style-sheet-service;1']
+		.getService(Ci.nsIStyleSheetService);
+var ios = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
 
-const FILENAME = "cam.css";
+var FILENAME = "cam.css";
 
 function getURIForFile(filepath) {
-  return ios.newURI(__SCRIPT_URI_SPEC__.replace("bootstrap.js", filepath), null, null);
+	return ios.newURI(__SCRIPT_URI_SPEC__.replace("bootstrap.js", filepath),
+			null, null);
 }
 
 function startup(data, reason) {
-  var uri = getURIForFile(FILENAME);
-  if(!sss.sheetRegistered(uri, sss.USER_SHEET)) {
-    sss.loadAndRegisterSheet(uri, sss.USER_SHEET);
-  }
+	var uri = getURIForFile(FILENAME);
+	if (!sss.sheetRegistered(uri, sss.USER_SHEET)) {
+		sss.loadAndRegisterSheet(uri, sss.USER_SHEET);
+	}
 }
 
 function shutdown(data, reason) {
-  if(reason == APP_SHUTDOWN)  return;
+	if (reason == APP_SHUTDOWN)
+		return;
 
-  var uri = getURIForFile(FILENAME);
-  if(sss.sheetRegistered(uri, sss.USER_SHEET)) {
-    sss.unregisterSheet(uri, sss.USER_SHEET);
-  }
+	var uri = getURIForFile(FILENAME);
+	if (sss.sheetRegistered(uri, sss.USER_SHEET)) {
+		sss.unregisterSheet(uri, sss.USER_SHEET);
+	}
 }
 
-function install() {}
-function uninstall() {}
+function install() {
+}
+function uninstall() {
+}
